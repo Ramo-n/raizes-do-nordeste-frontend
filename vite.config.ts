960963b7d5c2f07c80e3b287a7e1afbbd6c05cb2
Ramo-n <ramo.n@hotmail.com>
2026-09-17 19:36:19 -0300

    ci: configurar deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/raizes-do-nordeste-frontend/',
   plugins: [react()],
   resolve: {
     alias: {
